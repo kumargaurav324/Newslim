373a835ca36cdd40995ceae6ec802e9832bc4824
Kumar Gaurav <kumargaurav@Kumars-MacBook-Pro.local>
2017-05-27 12:21:28 +0530

    10 commit

--- test.php
+++ test.php
@@ -1,15 +1,5 @@
 <?php
-
-echo "hello world 2";
-
-if ( $_POST['payload'] ) {
     
-    echo $_POST['payload'];
+    shell_exec( 'cd /public_html/webhook_test/Newslim && git add . && git commit -m "just commit" && git pull origin master' );
     
-      shell_exec( 'cd /public_html/webhook_test/Newslim && git add . && git commit -m "just commit" && git pull origin master' );
-
-    
-  //shell_exec( 'cd /srv/www/git-repo/ && git reset --hard HEAD && git pull' );
-}
-
-?>
+    ?>
