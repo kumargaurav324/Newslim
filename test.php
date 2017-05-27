85541e9c5395dc177e22d6d9c200bb6c2f0f8832
Kumar Gaurav <kumargaurav@Kumars-MacBook-Pro.local>
2017-05-27 12:26:44 +0530

    12 commit

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
     
-    shell_exec( 'cd /public_html/webhook_test/Newslim && git add . && git commit -m "just commit" && git pull origin master' );
+    shell_exec( 'cd /home/getsport/public_html/webhook_test/Newslim && git add . && git commit -m "just commit" && git pull origin master' );
     
     ?>
