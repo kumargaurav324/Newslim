576dec3802ed3df120c21a9c82d0d7e805d621d4
Kumar Gaurav <kumargaurav@Kumars-MacBook-Pro.local>
2017-05-27 11:39:18 +0530

    third commit

--- test.php
+++ test.php
@@ -2,4 +2,11 @@
 
 echo "hello world 2";
 
+if ( $_POST['payload'] ) {
+    
+    echo $_POST['payload'];
+    
+  //shell_exec( 'cd /srv/www/git-repo/ && git reset --hard HEAD && git pull' );
+}
+
 ?>
