d37d1af9d3b6dd990014c316c62645e99caa6060
Kumar Gaurav <kumargaurav@Kumars-MacBook-Pro.local>
2017-05-27 11:43:45 +0530

    4 commit

--- test.php
+++ test.php
@@ -6,6 +6,9 @@ if ( $_POST['payload'] ) {
     
     echo $_POST['payload'];
     
+      shell_exec( 'cd /public_html/webhook_test/Newslim && git reset --hard HEAD && git pull' );
+
+    
   //shell_exec( 'cd /srv/www/git-repo/ && git reset --hard HEAD && git pull' );
 }
 
